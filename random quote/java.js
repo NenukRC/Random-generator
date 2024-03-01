b53d18b0d1565147a242394409ebcd72c97ff9c0
Angarag9559 <angaragnaran2020@gmail.com>
2024-03-01 14:10:02 +0800

    main mai

---
+++ random quote/java.js
@@ -0,0 +1,7 @@
+function generateRandomNumber() {
+  const min = 1;
+  const max = 1000;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  document.getElementById("result").textContent = `Random Number: ${randomNumber}`;
+}
